25795f7d90b4cad3ae3c140875f39756a956069d
Dale Cook <dalecook@dale.local>
2010-06-07 21:52:33 -0700

    added security changes and updated README

--- lib/twilio_mime.rb
+++ lib/twilio_mime.rb
@@ -14,14 +14,22 @@ class ActionController::Base
 
 private
 
+  #As a security measure to ensure that the request is actually coming from Twilio set a constant to be
+  #equal to your Twilio account guid. If the constant is assigned then the Twilio responses will only
+  #be returned if the guids match, even if the response type is explicitly requested
+  #i.e. myapp.com/foo/show/1.twiml
+  #would return a HTML response (the default).
+  #If the account guid constant is not explicity set then we make a best guess as to whether this request
+  #is coming from Twilio. In this case the response can be explicity requested using the twiml mime type.
   def is_twilio_request?
-    
     if defined?(TWILIO_ACCOUNT_GUID)
-      return params["AccountGuid"] == TWILIO_ACCOUNT_GUID || params["AccountSid"] == TWILIO_ACCOUNT_GUID
+      #does the request contain a valid account guid
+      is_twilio = (params["AccountGuid"] == TWILIO_ACCOUNT_GUID || params["AccountSid"] == TWILIO_ACCOUNT_GUID)
+      params[:format] = "html" if params[:format] == "twiml" && !is_twilio
+      return is_twilio
     else
       return (params.include?("AccountSid") && params.include?("SmsSid")) || (params.include?("AccountGuid") && params.include?("CallGuid"))
-    end
-    
+    end    
   end
   
 end
